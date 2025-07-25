40fa6fd12c6469ed7e81dd18a8802d39b326536a
Rohit-0508 <rohitranaut91@gmail.com>
2025-07-25 16:26:58 +0530

    fixed the signup route

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom'
 
 export default function Login() {
   const { login } = useAuth();
@@ -26,7 +27,7 @@ export default function Login() {
         <input type="password" placeholder="Password" className="input" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button className="bg-blue-600 text-white px-4 py-2 rounded w-full">Login</button>
         <p className="mt-4 text-center">
-          Don't have an account? <a href="/signup" className="text-blue-600">Sign up</a>
+          Don't have an account? <Link to="/signup" className="text-blue-600">Sign up</Link>
         </p>
       </form>
 
